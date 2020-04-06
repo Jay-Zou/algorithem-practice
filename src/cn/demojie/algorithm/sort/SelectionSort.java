25dff72fd40d3d0e95aadc63e70cf18f2fa8a0a9
demojie <1426134543@qq.com>
2020-04-07 01:33:21 +0800

    Add binary selection sort.

--- src/cn/demojie/algorithm/sort/SelectionSort.java
+++ src/cn/demojie/algorithm/sort/SelectionSort.java
@@ -12,8 +12,10 @@ public class SelectionSort {
         Character[] array = {'S', 'E', 'L', 'E', 'C', 'T', 'I', 'O', 'N', 'S', 'O', 'R', 'T', 'E', 'X', 'A', 'M', 'P', 'L', 'E'};
         Utils.printArr(array);
 
-        selectionSort.selectionSort(array);
+        selectionSort.binarySelectionSort(array);
+//        selectionSort.selectionSort(array);
         // [A, C, E, E, E, E, I, L, L, M, N, O, O, P, R, S, S, T, T, X]
+        // [A, C, E, E, E, I, E, L, L, M, N, O, O, P, R, S, S, T, T, X]
         Utils.printArr(array);
     }
 
@@ -40,6 +42,67 @@ public class SelectionSort {
                 }
             }
             Utils.swap(array, i, minIndex);
+        }
+    }
+
+    /**
+     * 二元选择排序 O(n^2) 不稳定
+     * <p>
+     * 每次都找到最大值和最小值
+     *
+     * @param array
+     */
+    private void binarySelectionSort(Comparable[] array) {
+        if (null == array || array.length == 0) {
+            throw new NullPointerException();
+        }
+        int len = array.length;
+        for (int i = 0; i < len / 2; i++) {
+            int minIndex = i;
+            int maxIndex = len - 1 - i;
+            for (int j = i + 1; j <= len - 1 - i; j++) {
+                if (array[j].compareTo(array[minIndex]) < 0) {
+                    minIndex = j;
+                }
+                if (array[j].compareTo(array[maxIndex]) > 0) {
+                    maxIndex = j;
+                }
+            }
+            Utils.swap(array, i, minIndex);
+            Utils.swap(array, len - 1 - i, maxIndex);
+        }
+    }
+    private void binarySelectionSort2(Comparable[] array) {
+        if (null == array || array.length == 0) {
+            throw new NullPointerException();
+        }
+        int len = array.length;
+        for (int i = 0; i < len / 2; i++) {
+            int minIndex = i;
+            int maxIndex = len - 1 - i;
+            for (int j = i + 1; j < len - 1 - i; j++) {
+                if (array[j].compareTo(array[maxIndex]) > 0) {
+                    maxIndex = j;
+                    continue;
+                }
+                if (array[j].compareTo(array[minIndex]) < 0) {
+                    minIndex = j;
+                }
+            }
+            if (minIndex != i) {
+                Utils.swap(array, i, minIndex);
+            }
+            if (minIndex == len - 1 - i && maxIndex == i) {
+                continue;
+            }
+            // 如果第一个位置为max，那么min交换后，把max给换掉了
+            if (maxIndex == i) {
+                maxIndex = minIndex;
+            }
+            if (maxIndex != len - 1 - i) {
+                Utils.swap(array, len - 1 - i, maxIndex);
+            }
+            // TODO 算法还是有问题
         }
     }
 }
